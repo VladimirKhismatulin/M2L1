c6df8522950e7cd7900ba0a8452232622456440a
VladimirKhismatulin <129597519+VladimirKhismatulin@users.noreply.github.com>
2023-05-04 23:00:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import random
+import requests
 import os
 from discord.ext import commands
 
@@ -8,6 +9,7 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 images = os.listdir('images')
+rares = os.listdir('rares')
 
 @bot.event
 async def on_ready():
@@ -19,10 +21,28 @@ async def hello(ctx):
 
 @bot.command()
 async def mem(ctx):
-    img = random.choice(images)
-    with open(f'images/{img}', 'rb') as f:
-        picture = discord.File(f)
-        await ctx.send(file = picture)
+    if random.randint(1, 5) != 5:
+        img = random.choice(images)
+        with open(f'images/{img}', 'rb') as f:
+            picture = discord.File(f)
+            await ctx.send(file = picture)
+    else:
+        img = random.choice(rares)
+        with open(f'rares/{img}', 'rb') as f:
+            picture = discord.File(f)
+            await ctx.send(file = picture)
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('fox')
+async def fox(ctx):
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
